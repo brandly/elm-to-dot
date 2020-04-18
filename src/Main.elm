5b313e0c2fa064b5d5a379a9655c7c2e0b8d7bdf
brandly <brandly13@gmail.com>
2020-04-18 18:06:39 -0400

    take deadEndsToString implementation from PR, fixes #4

--- src/Main.elm
+++ src/Main.elm
@@ -131,7 +131,7 @@ update options msg model =
                         )
 
                 Err e ->
-                    ( model, Native.Log.line <| E.string ("failed to parse: " ++ Parser.deadEndsToString e) )
+                    ( model, Native.Log.line <| E.string ("failed to parse: " ++ deadEndsToString e) )
 
         ( Crawling state, FileError (Ok { code, message, path }) ) ->
             if code == "ENOENT" then
@@ -203,3 +203,61 @@ subscriptions model =
 
         _ ->
             Sub.none
+
+
+deadEndsToString : List Parser.DeadEnd -> String
+deadEndsToString deadEnds =
+    -- Parser.deadEndsToString is broken/neglected. Here's a patch from this PR:
+    -- https://github.com/elm/parser/pull/38
+    let
+        deadEndToString : Parser.DeadEnd -> String
+        deadEndToString deadEnd =
+            let
+                position : String
+                position =
+                    "row:" ++ String.fromInt deadEnd.row ++ " col:" ++ String.fromInt deadEnd.col ++ "\n"
+            in
+            case deadEnd.problem of
+                Parser.Expecting str ->
+                    "Expecting " ++ str ++ "at " ++ position
+
+                Parser.ExpectingInt ->
+                    "ExpectingInt at " ++ position
+
+                Parser.ExpectingHex ->
+                    "ExpectingHex at " ++ position
+
+                Parser.ExpectingOctal ->
+                    "ExpectingOctal at " ++ position
+
+                Parser.ExpectingBinary ->
+                    "ExpectingBinary at " ++ position
+
+                Parser.ExpectingFloat ->
+                    "ExpectingFloat at " ++ position
+
+                Parser.ExpectingNumber ->
+                    "ExpectingNumber at " ++ position
+
+                Parser.ExpectingVariable ->
+                    "ExpectingVariable at " ++ position
+
+                Parser.ExpectingSymbol str ->
+                    "ExpectingSymbol " ++ str ++ " at " ++ position
+
+                Parser.ExpectingKeyword str ->
+                    "ExpectingKeyword " ++ str ++ "at " ++ position
+
+                Parser.ExpectingEnd ->
+                    "ExpectingEnd at " ++ position
+
+                Parser.UnexpectedChar ->
+                    "UnexpectedChar at " ++ position
+
+                Parser.Problem str ->
+                    "ProblemString " ++ str ++ " at " ++ position
+
+                Parser.BadRepeat ->
+                    "BadRepeat at " ++ position
+    in
+    List.foldl (++) "" (List.map deadEndToString deadEnds)
